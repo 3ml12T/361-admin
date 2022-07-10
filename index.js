24fce1a7588fc42d50192b593275223034ebe4a2
marthamatsui <55360376+marthamatsui@users.noreply.github.com>
2022-07-10 05:17:48 -0400

    added real graph images

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ function changeTempGraph() {
 // the source path on the image is changed to match the corresponding radio button
 
   if(document.getElementById('temp-e5').checked) {
-  document.getElementById("temp-graph-img").src = "images/e5-classroom.jpg";
+  document.getElementById("temp-graph-img").src = "images/temperature-e5.png";
     }
 
   else if(document.getElementById('temp-syde-lounge').checked) {
-  document.getElementById("temp-graph-img").src = "images/syde-lounge.jpg";
+  document.getElementById("temp-graph-img").src = "images/temperature-syde-lounge.png";
     }
 
   else if (document.getElementById('temp-dc-library').checked) {
-  document.getElementById("temp-graph-img").src = "images/dc-library.jpg";
+  document.getElementById("temp-graph-img").src = "images/temperature-dc-library.png";
     }
 
   else if (document.getElementById('temp-all-locations').checked) {
-  document.getElementById("temp-graph-img").src = "images/all-locations.jpg";
+  document.getElementById("temp-graph-img").src = "images/temperature-all-locations.png";
     }
 
 };
@@ -37,19 +37,19 @@ function changeTempGraph() {
 function changeHumGraph() {
 
   if(document.getElementById('hum-e5').checked) {
-  document.getElementById("hum-graph-img").src = "images/e5-classroom.jpg";
+  document.getElementById("hum-graph-img").src = "images/humidity-e5.png";
     }
 
   else if(document.getElementById('hum-syde-lounge').checked) {
-  document.getElementById("hum-graph-img").src = "images/syde-lounge.jpg";
+  document.getElementById("hum-graph-img").src = "images/humidity-syde-lounge.png";
     }
 
   else if (document.getElementById('hum-dc-library').checked) {
-  document.getElementById("hum-graph-img").src = "images/dc-library.jpg";
+  document.getElementById("hum-graph-img").src = "images/humidity-dc-library.png";
     }
 
   else if (document.getElementById('hum-all-locations').checked) {
-  document.getElementById("hum-graph-img").src = "images/all-locations.jpg";
+  document.getElementById("hum-graph-img").src = "images/humidity-all-locations.png";
     }
 
 };
@@ -60,19 +60,19 @@ function changeHumGraph() {
 function changeLightGraph() {
 
   if(document.getElementById('light-e5').checked) {
-  document.getElementById("light-graph-img").src = "images/e5-classroom.jpg";
+  document.getElementById("light-graph-img").src = "images/light-e5.png";
     }
 
   else if(document.getElementById('light-syde-lounge').checked) {
-  document.getElementById("light-graph-img").src = "images/syde-lounge.jpg";
+  document.getElementById("light-graph-img").src = "images/light-syde-lounge.png";
     }
 
   else if (document.getElementById('light-dc-library').checked) {
-  document.getElementById("light-graph-img").src = "images/dc-library.jpg";
+  document.getElementById("light-graph-img").src = "images/light-dc-library.png";
     }
 
   else if (document.getElementById('light-all-locations').checked) {
-  document.getElementById("light-graph-img").src = "images/all-locations.jpg";
+  document.getElementById("light-graph-img").src = "images/light-all-locations.png";
     }
 
 };
